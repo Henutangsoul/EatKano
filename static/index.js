80908d4d07ea47c9232db2c90337e2df8861c124
Henutangsoul <98441704+Henutangsoul@users.noreply.github.com>
2022-01-26 12:49:06 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -350,11 +350,11 @@
             return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
         }
         SubmitResults();
-        if (score <= 49) return '试着好好练一下？';
-        if (score <= 99) return 'TCL';
-        if (score <= 149) return 'TQL';
-        if (score <= 199) return '您';
-        return '人？';
+        if (score <= 49) return '没手是吧？';
+        if (score <= 99) return '您？';
+        if (score <= 149) return 'tql';
+        if (score <= 199) return '狗囧在世';
+        return '草？';
     }
 
     w.toStr = function(obj) {
